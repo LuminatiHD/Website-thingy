a42b3a375f403667c63ddacd6f7cfaed8103d22e
LuminatiHD <yoan.mueller@protonmail.com>
2022-05-11 11:05:51 +0200

    test

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
-let count = document.getElementById("increment-btn")
-
-let a = 0
-
-function inc() {
-    a+=1
+/*use strict*/
+function submitform(){
+    url = document.getElementsByName("link")[0].value
+    hash = document.getElementsByName("hash")[0].value      
+    var fs = require('fs')
+    alert(hash)
+    var htmlContent = '<html>Whatever</html>'
+    fs.writeFile('./redirect/'+hash+'.html', htmlContent)
 }
 
